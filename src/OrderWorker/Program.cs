b0b2606a17ee3b86fb8be2c872d3ad96e2d0c00f
Chih Yun (Queena) Wu <QWu@ninepeakssolutions.com>
2026-06-10 13:27:11 -0700

    Add Application Insights to all services

--- src/OrderWorker/Program.cs
+++ src/OrderWorker/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using OrderWorker;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Add Application Insights
+builder.Services.AddApplicationInsightsTelemetryWorkerService();
 
 // Make configuration available to Worker
 builder.Services.AddSingleton(builder.Configuration);
